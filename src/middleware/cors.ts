9a276ced0c99aed7320d939c9cbe857aae6bebd7
Mehmet Bajin <10424048+mehmetbajin@users.noreply.github.com>
2020-04-10 09:40:29 -0400

    fix: enable cors (#127)

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,15 +1,15 @@
 import cors from 'cors';
 import { RequestHandler } from 'express';
-// import { appConfig } from '../config';
+import { appConfig } from '../config';
 
-// export const middleware = (): RequestHandler =>
-//   appConfig.environment === 'production'
-//     ? cors({
-//         origin: (origin, callback) =>
-//           appConfig.corsWhitelist.includes(origin)
-//             ? callback(undefined, true)
-//             : callback(new Error(`"${origin}" not allowed by CORS.`))
-//       })
-//     : cors();
+export const middleware = (): RequestHandler =>
+  appConfig.environment === 'production' || appConfig.environment === 'staging'
+    ? cors({
+        origin: (origin, callback) =>
+          appConfig.corsWhitelist.includes(origin)
+            ? callback(undefined, true)
+            : callback(new Error(`"${origin}" not allowed by CORS.`), false),
+      })
+    : cors();
 
-export const middleware = (): RequestHandler => cors();
+// export const middleware = (): RequestHandler => cors();
